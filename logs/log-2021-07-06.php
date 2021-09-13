5beecac55bdbf01c335d3e2547f581cfd009adb1
lukmnh <86191995+lukmnh@users.noreply.github.com>
2021-09-13 17:36:42 +0700

    Add files via upload

---
+++ logs/log-2021-07-06.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-07-06 16:09:27 --> Severity: Warning --> Undefined variable $new_image C:\xampp\htdocs\siakad\application\controllers\data_guru.php 162
+ERROR - 2021-07-06 16:23:03 --> Some variable did not contain a value.
